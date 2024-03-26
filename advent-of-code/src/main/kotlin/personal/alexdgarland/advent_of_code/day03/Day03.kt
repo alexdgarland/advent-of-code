1c676e17bb6cf58b2eadee1c53e943c11e0cb86e
Alex Garland <alex.d.garland@gmail.com>
2024-03-26 17:45:24 +0000

    Day 3 part 2 solution
    Good-enough solution - structure and state handling aren't especially maintainable but have made up my mind it's not worth over-factoring code that is for such a throw-away problem set

--- advent-of-code/src/main/kotlin/personal/alexdgarland/advent_of_code/day03/Day03.kt
+++ advent-of-code/src/main/kotlin/personal/alexdgarland/advent_of_code/day03/Day03.kt
@@ -2,30 +2,66 @@ package personal.alexdgarland.advent_of_code.day03
 
 import personal.alexdgarland.advent_of_code.Input
 
-private fun isSymbol(line: String?, charIndex: Int): Boolean {
-    if (line == null || charIndex < 0 || charIndex > line.lastIndex) {
-        return false
+data class Coordinate(val lineIndex: Int, val charIndex: Int) {
+    fun outOfBounds(grid: List<String>): Boolean {
+        return lineIndex < 0 ||
+                lineIndex > grid.lastIndex ||
+                charIndex < 0 ||
+                charIndex > grid[lineIndex].lastIndex
     }
-    return line[charIndex] != '.' && !line[charIndex].isDigit()
+
+    override fun toString(): String {
+        return "($lineIndex, $charIndex)"
+    }
 }
 
-fun getPartNumbersFromLine(grid: List<String>, lineIndex: Int): List<Int> {
+data class PartNumberInformation(val number: Int, val gearCandidateCoordinates: Set<Coordinate>) {
+
+    override fun toString(): String {
+        return "Part Number $number - coords [${gearCandidateCoordinates.joinToString(", ")}]"
+    }
+}
+
+fun getPartNumbersFromLine(grid: List<String>, lineIndex: Int): List<PartNumberInformation> {
     // Vals that are read-only/ fully immutable within scope of function
     val currLine: String = grid[lineIndex]
-    val prevLine: String? = if (lineIndex == 0) null else grid[lineIndex-1]
-    val nextLine: String? = if (lineIndex == grid.lastIndex) null else grid[lineIndex+1]
     // Immutable refs, mutable objects
-    val partsList = mutableListOf<Int>()
+    val partsList = mutableListOf<PartNumberInformation>()
     val builder = StringBuilder()
+    val gearCandidateCoordinates = mutableSetOf<Coordinate>()
     // Mutable vars
     var inWord = false
     var sawSymbol = false
 
+    // These "checkForSymbol" functions are a real mess of return values and side effects;
+    // it might be overkill for this problem, but handling of state could be a lot clearer and more consistent.
+
+    fun checkForSymbol(coordinate: Coordinate): Boolean {
+        if (coordinate.outOfBounds(grid)) {
+            return false
+        }
+        val char = grid[coordinate.lineIndex][coordinate.charIndex]
+        if(char == '*') {
+            gearCandidateCoordinates.add(coordinate)
+            return true
+        }
+        return char != '.' && !char.isDigit()
+    }
+
+    fun checkForSymbols(coordinates: List<Coordinate>) {
+        for (coord in coordinates) {
+            if (checkForSymbol(coord)) {
+                sawSymbol = true
+            }
+        }
+    }
+
     fun flush() {
         if(sawSymbol) {
-            partsList.add(builder.toString().toInt())
+            partsList.add(PartNumberInformation(builder.toString().toInt(), gearCandidateCoordinates.toSet()))
         }
         builder.clear()
+        gearCandidateCoordinates.clear()
         inWord = false
         sawSymbol = false
     }
@@ -33,25 +69,33 @@ fun getPartNumbersFromLine(grid: List<String>, lineIndex: Int): List<Int> {
     for(charIndex in currLine.indices) {
         val currentChar = currLine[charIndex]
         if(inWord) {
-            sawSymbol = sawSymbol || isSymbol(prevLine, charIndex) || isSymbol(nextLine, charIndex)
+            checkForSymbols(
+                listOf(
+                    Coordinate(lineIndex-1, charIndex),
+                    Coordinate(lineIndex+1, charIndex)
+                )
+            )
             if(currentChar.isDigit()) {
                 // Keep building current part number
                 builder.append(currentChar)
             }
             else {
-                // Flush current part number to list if valid
-                sawSymbol = sawSymbol || isSymbol(currLine, charIndex)
+                checkForSymbols(listOf(Coordinate(lineIndex, charIndex)))
                 flush()
             }
         } else {
             if(currentChar.isDigit()) {
                 // Start a new part number
                 inWord = true
-                sawSymbol = isSymbol(prevLine, charIndex - 1) ||
-                        isSymbol(currLine, charIndex - 1) ||
-                        isSymbol(nextLine, charIndex - 1) ||
-                        isSymbol(prevLine, charIndex) ||
-                        isSymbol(nextLine, charIndex)
+                checkForSymbols(
+                    listOf(
+                        Coordinate(lineIndex-1, charIndex - 1),
+                        Coordinate(lineIndex, charIndex - 1),
+                        Coordinate(lineIndex+1, charIndex - 1),
+                        Coordinate(lineIndex-1, charIndex),
+                        Coordinate(lineIndex+1, charIndex)
+                    )
+                )
                 builder.append(currentChar)
             }
         }
@@ -62,7 +106,7 @@ fun getPartNumbersFromLine(grid: List<String>, lineIndex: Int): List<Int> {
     return partsList
 }
 
-fun getPartNumbersFromGrid(grid: List<String>): List<Int> {
+fun getPartNumbersFromGrid(grid: List<String>): List<PartNumberInformation> {
     return grid.indices.flatMap { index -> getPartNumbersFromLine(grid, index) }
 }
 
@@ -70,8 +114,24 @@ object Solution {
 
     fun run() {
         val grid = Input.getLines("03").toList()
-        val result = getPartNumbersFromGrid(grid).sum()
-        print(" --- $result --- ")
+
+        val results = getPartNumbersFromGrid(grid)
+
+        val partNumbersResult = results.sumOf { it.number }
+        println(" --- $partNumbersResult --- ")
+
+        val coordsPartDict = mutableMapOf<Coordinate, MutableSet<Int>>()
+        for(r in results) {
+            for(coord in r.gearCandidateCoordinates) {
+                (coordsPartDict.getOrPut(coord){ mutableSetOf() }).add(r.number)
+            }
+        }
+        val gearsResult = coordsPartDict
+            .filter { it.value.size == 2 }
+            .map{it.value.reduce(Int::times)}
+            .sum()
+        println(" --- $gearsResult --- ")
+
     }
 
 }
